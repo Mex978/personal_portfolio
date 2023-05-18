4c87e8751f8e1621088d2f62351cff0e92dc695c
Max Lima <max.lima2@gmail.com>
2023-05-18 12:54:52 -0300

    add firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_portfolio/main_app.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   setPathUrlStrategy();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
